02a32b00bf8e5f6e6ad790cc07304e8ead2b40d7
Vasyl Hvozdulych <hvozdulych@gmail.com>
2021-10-24 17:19:37 +0300

    Add inline-source-map
    https://webpack.js.org/guides/development/#using-source-maps

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].bundle.js'
     },
+    devtool: 'inline-source-map',
     devServer: {
         static: './dist',
         open: true,
