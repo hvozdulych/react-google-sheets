97c55e28aa65618ed2cebeb6d1eb3687c3c2ecf9
Vasyl Hvozdulych <hvozdulych@gmail.com>
2021-10-31 02:17:35 +0300

    Update app to use form and new styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,20 @@
 import React from "react"
+import { Container } from "@mui/material";
 import logo from './logo.svg';
 import './App.css';
+import { Form } from "./components/form"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          Test
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="app">
+            <header className="app-header">
+                <img src={logo} className="app-logo" alt="logo" />
+            </header>
+            <Container className="app-content">
+                <Form/>
+            </Container>
+        </div>
+    );
 }
 
 export default App;
